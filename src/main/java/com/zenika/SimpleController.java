907a70d95a004cec4480e57c947134a061941a89
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2013-01-15 12:07:38 +0100

    added simple controller and simple REST controller tests

---
+++ src/main/java/com/zenika/SimpleController.java
@@ -0,0 +1,20 @@
+package com.zenika;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ *
+ */
+@Controller
+public class SimpleController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+}
